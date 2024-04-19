963f1509ba2a4cd2f28a0b0a6e73ef49091d245a
GabeCorsova <148981238+GabeCorsova@users.noreply.github.com>
2024-04-19 10:17:27 -0400

    Addng - Unistalling LTech agent

---
+++ LTechAgent unInstallation Script.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+## Uninstall Script for LTSAgents
+## By Gabriel Marcelino - Senior Apple Technician
+
+## Uninstall LTS
+sh /usr/local/ltechagent/uninstaller.sh
+
+#Remote Connect wise if exist
+rm -rf /Applications/connectwisecontrol*
+rm -rf /Library/LaunchDaemons/connectwisecontrol*
+rm -rf /Library/LaunchAgents/connectwisecontrol*
+rm -rf /opt/screenconnect*
+rm -rf /Library/LaunchDaemons/screenconnect*
+rm -rf /Library/LaunchAgents/screenconnect*
+
+
+#Clean it up old installer
+rm -rf "/private/var/tmp/LTSAgent"
+
+exit 0		## Success
